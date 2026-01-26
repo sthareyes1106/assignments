b836f7ff77d0ef9c0759303c69d00c1005d22669
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-26 11:37:52 -0500

    Added currentCoordinates function

--- spacecraft.js
+++ spacecraft.js
@@ -10,10 +10,13 @@ activateHyperDrive();
 // TODO: Create an arrow function named `scanForLife` that implicitly returns `"No lifeforms detected"` without using curly braces. Print the result of calling `scanForLife`.
 
 const scanForLife = () => console.log("No lifeforms detected");
-scanForLife();
+console.log(scanForLife());
 
 /* Task 3: Implicit Return with Objects: Log Coordinates */
 // TODO: Write an arrow function named `currentCoordinates` that returns an object with properties `x`, `y`, and `z`, representing coordinates in space. Use implicit return. Print the returned object from `currentCoordinates`.
+
+const currentCoordinates = () => ({ x: 27, y: 32, z: 76 });
+console.log(currentCoordinates());
 
 /* Task 4: Understanding `this`: Message from Home Base */
 // TODO: Inside an object named `spacecraft`, create a method named `receiveMessage` using arrow function syntax. This method should log `"Message received: "` followed by a message it receives as a parameter. Directly call `receiveMessage` within `spacecraft` and observe. Observe and explain the behavior of `this` in this context as a comment.
