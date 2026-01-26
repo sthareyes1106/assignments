b908da34ab9c19097d611a8eb9769372f5617000
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-26 11:13:21 -0500

    Added scanForLife function

---
+++ spacecraft.js
@@ -0,0 +1,23 @@
+/* Task 1: No Parameters: Activate Hyperdrive */
+// TODO: Write an arrow function named `activateHyperdrive` with no parameters that print `"Hyperdrive activated!"` to the console. Call `activateHyperdrive` to test it.
+
+const activateHyperDrive = (/*no parameters*/) => {
+  return console.log("Hyperdrive activated!");
+};
+activateHyperDrive();
+
+/* Task 2: Implicit Return: Scan for Lifeforms */
+// TODO: Create an arrow function named `scanForLife` that implicitly returns `"No lifeforms detected"` without using curly braces. Print the result of calling `scanForLife`.
+
+const scanForLife = () => console.log("No lifeforms detected");
+scanForLife();
+
+/* Task 3: Implicit Return with Objects: Log Coordinates */
+// TODO: Write an arrow function named `currentCoordinates` that returns an object with properties `x`, `y`, and `z`, representing coordinates in space. Use implicit return. Print the returned object from `currentCoordinates`.
+
+/* Task 4: Understanding `this`: Message from Home Base */
+// TODO: Inside an object named `spacecraft`, create a method named `receiveMessage` using arrow function syntax. This method should log `"Message received: "` followed by a message it receives as a parameter. Directly call `receiveMessage` within `spacecraft` and observe. Observe and explain the behavior of `this` in this context as a comment.
+/*
+ * Observations:
+ * TODO: Explain here.
+ */
