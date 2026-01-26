91c208e48a5360e865712e0b3df49d52c568c534
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-26 10:42:21 -0500

    Reassigned destination variable

--- time-travel.js
+++ time-travel.js
@@ -5,6 +5,8 @@ console.log(destination);
 
 /* Task 2: Change the Destination */
 // TODO: Now, change the `destination` variable to `"Medieval Europe"`. Print the new destination to the console.
+destination = "Medieval Europe";
+console.log(destination);
 
 /* Task 3: Declare a Constant Travel Date */
 // TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
