6574cad23b6002e17f1fd46ad31dde7578d51aa9
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-26 10:51:39 -0500

    Declared timeMachineModel with var to observe variable hoisting and wrote a comment for observations

--- time-travel.js
+++ time-travel.js
@@ -1,28 +1,27 @@
 /* Task 1: Declare a Destination Variable */
-// TODO: Use `let` to declare a variable named `destination` and assign it the value `"Ancient Egypt"`. Print the destination to the console.
 let destination = "Ancient Egypt";
 console.log(destination);
 
 /* Task 2: Change the Destination */
-// TODO: Now, change the `destination` variable to `"Medieval Europe"`. Print the new destination to the console.
 destination = "Medieval Europe";
 console.log(destination);
 
 /* Task 3: Declare a Constant Travel Date */
-// TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
 const travelDate = "2024-03-15";
 console.log(travelDate);
 
 // travelDate = "2025-01-26";
+
 /*
  * Observations:
- * TODO: Explain here.
  * I got a type error. I can't reassign a constant variable.
  */
 
 /* Task 4: Experiment with Variable Hoisting */
-// TODO: Before declaring any variable, try to print a variable named `timeMachineModel` to the console. Then, declare `timeMachineModel` using `var` and assign it the value `"T-800"`. Observe and explain what happens as a comment.
+console.log(timeMachineModel);
+var timeMachineModel = "T-800";
+
 /*
  * Observations:
- * TODO: Explain here.
+ * Initially, I got a reference error, since timeMachineModel wasn't defined. Once I defined it with `var`, it printed out `undefined`. When using `var`, the declaration of the variable is "moved to the top" of the doc, so the variable "exists" but without a value (hence the undefined value). Not sure if that made any sense.
  */
