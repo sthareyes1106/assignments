641a1d77ce51045525c7c9a1478891e77de802df
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-27 17:23:13 -0500

    Updated comments

--- science-lab.js
+++ science-lab.js
@@ -1,12 +1,11 @@
 /* Task 1: Compile Participant Details with Shorthand Property Names */
-// TODO: Construct an object named `participant` with properties for `name`, `age`, and `studyField`. Utilize shorthand property names to simplify your code.
 
 // Example participant variables
 const name = "Hank";
 const age = 45;
 const studyField = "Biology";
 
-// Starting participant object
+// Starting participant object utilizing object shorthand
 const participant = {
   name,
   age,
@@ -14,7 +13,7 @@ const participant = {
 };
 
 /* Task 2: Implement a Shorthand Function for Participant Info */
-// TODO: Copy the `participant` object by adding a shorthand method named `displayInfo` that prints the participant's details using `this` and a template string.
+// Created copy object with displayInfo method
 
 const newParticipant = {
   ...participant,
@@ -24,11 +23,11 @@ const newParticipant = {
     );
   },
 };
-
+// Testing displayInfo method
 newParticipant.displayInfo();
 
 /* Task 3: Implement a Same Shorthand Arrow Function for Participant Info */
-// TODO: Echo the above task with an arrow function. Observe the behavior of `this` and explain your findings.
+// Created another object which mimcs the first, but using an arrow function
 
 const secondParticipant = {
   ...participant,
@@ -38,16 +37,17 @@ const secondParticipant = {
     );
   },
 };
+
+// Testing displayInfo method when using an arrow function
 secondParticipant.displayInfo();
 
 /*
  * Observations:
- * TODO: Explain here.
  * Arrow functions behave differently in objects; they don't make their own context for `this`... So when the statement prints, the values are blank (for `name`) or undefined (for `age` and `studyField`). It's usually a pretty bad idea to use arrow functions inside of objects.
  */
 
 /* Task 4: Using Computed Property Names */
-// TODO: Implement a function named `updateParticipantInfo` that takes a property name and value as arguments alongside an object and returns a new object with that property dynamically set.
+// Added a function to update a dynamic property and value, which returns the updated object
 
 function updateParticipantInfo(propName, val, obj) {
   const updatedObj = {
@@ -58,4 +58,5 @@ function updateParticipantInfo(propName, val, obj) {
   return updatedObj;
 }
 
+// Checking the returned object to make sure all is well
 console.log(updateParticipantInfo("name", "Hank Green", participant));
