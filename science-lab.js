5d9494eff1df95ca12ec3991d755497a28174115
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-27 17:02:03 -0500

    Added secondParticipant object to observe arrow functions in objects

--- science-lab.js
+++ science-lab.js
@@ -25,13 +25,25 @@ const newParticipant = {
   },
 };
 
-// newParticipant.displayInfo();
+newParticipant.displayInfo();
 
 /* Task 3: Implement a Same Shorthand Arrow Function for Participant Info */
 // TODO: Echo the above task with an arrow function. Observe the behavior of `this` and explain your findings.
+
+const secondParticipant = {
+  ...participant,
+  displayInfo: () => {
+    console.log(
+      `This participant, ${this.name}, is ${this.age} years old, and works in ${this.studyField}.`,
+    );
+  },
+};
+secondParticipant.displayInfo();
+
 /*
  * Observations:
  * TODO: Explain here.
+ * Arrow functions behave differently in objects; they don't make their own context for `this`... So when the statement prints, the values are blank (for `name`) or undefined (for `age` and `studyField`). It's usually a pretty bad idea to use arrow functions inside of objects.
  */
 
 /* Task 4: Using Computed Property Names */
