6df9e197604cfa7f6be8123218f543dc231483b2
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-27 14:18:43 -0500

    Added science-lab.js file

---
+++ science-lab.js
@@ -0,0 +1,15 @@
+/* Task 1: Compile Participant Details with Shorthand Property Names */
+// TODO: Construct an object named `participant` with properties for `name`, `age`, and `studyField`. Utilize shorthand property names to simplify your code.
+
+/* Task 2: Implement a Shorthand Function for Participant Info */
+// TODO: Copy the `participant` object by adding a shorthand method named `displayInfo` that prints the participant's details using `this` and a template string.
+
+/* Task 3: Implement a Same Shorthand Arrow Function for Participant Info */
+// TODO: Echo the above task with an arrow function. Observe the behavior of `this` and explain your findings.
+/*
+ * Observations:
+ * TODO: Explain here.
+ */
+
+/* Task 4: Using Computed Property Names */
+// TODO: Implement a function named `updateParticipantInfo` that takes a property name and value as arguments alongside an object and returns a new object with that property dynamically set.
