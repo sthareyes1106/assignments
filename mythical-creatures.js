138b4dffbb949dff1c1c65e44325b24277585970
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-28 17:17:07 -0500

    Added the lastSeenCreature variable

--- mythical-creatures.js
+++ mythical-creatures.js
@@ -6,13 +6,23 @@ const mythicalCreatures = [
   { name: "Kraken", type: "Water", lastSeen: "Abyssal Depths" },
 ];
 
+// Using `find` to find the first water-type creature
 const firstWater = mythicalCreatures.find(function (creature) {
   return creature.type === "Water";
 });
 console.log(firstWater);
 console.log(`The first creature with a water type is the ${firstWater.name}.`);
 
+// Using `findIndex` to find the index of the Griffin object
 const griffinIdx = mythicalCreatures.findIndex(function (creature) {
   return creature.name === "Griffin";
 });
 console.log(`The Griffin is at index ${griffinIdx}.`);
+
+// Assuming that the most recently seen creature is at the top...
+const lastSeenCreature = mythicalCreatures.find(function () {
+  return mythicalCreatures[0];
+});
+
+console.log(lastSeenCreature);
+console.log(`The last seen creature is the ${lastSeenCreature.name}!`);
