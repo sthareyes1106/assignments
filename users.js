cca4370fda12ebfdcea1d1a5b93c3e4e2e68d169
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-28 13:19:32 -0500

    Added users.js file

---
+++ users.js
@@ -0,0 +1,5 @@
+const users = [
+	{firstName: "Alice", lastName: "Johnson", points: 120},
+	{firstName: "Bob", lastName: "Smith", points: 99},
+	{firstName: "Charlie", lastName: "Brown", points: 180}
+];
