f3dd0eaf92044eb5d839856eb090a338b95326d7
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-28 17:05:20 -0500

    Added mythical-creatures.js file

---
+++ mythical-creatures.js
@@ -0,0 +1,7 @@
+const mythicalCreatures = [
+  { name: "Dragon", type: "Fire", lastSeen: "Volcano Valley" },
+  { name: "Mermaid", type: "Water", lastSeen: "Coral Caves" },
+  { name: "Unicorn", type: "Land", lastSeen: "Enchanted Forest" },
+  { name: "Griffin", type: "Air", lastSeen: "Highwind Mountains" },
+  { name: "Kraken", type: "Water", lastSeen: "Abyssal Depths" },
+];
