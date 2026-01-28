445c507989c2280e056bb18ff54dd10a6ee84ce4
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-28 13:35:25 -0500

    Added updatedUsers variable

--- users.js
+++ users.js
@@ -1,5 +1,16 @@
 const users = [
-	{firstName: "Alice", lastName: "Johnson", points: 120},
-	{firstName: "Bob", lastName: "Smith", points: 99},
-	{firstName: "Charlie", lastName: "Brown", points: 180}
+  { firstName: "Alice", lastName: "Johnson", points: 120 },
+  { firstName: "Bob", lastName: "Smith", points: 99 },
+  { firstName: "Charlie", lastName: "Brown", points: 180 },
 ];
+
+const updatedUsers = users.map(function (user) {
+  return {
+    // Add first and last names together
+    fullName: user.firstName + " " + user.lastName,
+    //Create membership status using ternary operator
+    membershipStatus: user.points > 100 ? "Premium" : "Standard",
+  };
+});
+
+console.log(updatedUsers);
