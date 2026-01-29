70a7ae681624a8f40acd3388dfd53f5f285e125c
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-29 16:02:05 -0500

    Called inventory.mjs functions in storeManager.mjs

--- modules/named-exports-and-imports/storeManager.mjs
+++ modules/named-exports-and-imports/storeManager.mjs
@@ -1,1 +1,14 @@
 // import functions from inventory.mjs to demostrate adding, removing, and listing items in the inventory
+
+import { addItem, removeItem, listItems } from "./inventory.mjs";
+
+addItem("notebook");
+addItem("broken egg");
+addItem("epic book");
+addItem("baby aligator");
+
+removeItem("broken egg");
+removeItem("notebook");
+
+addItem("hollywood trophy");
+listItems();
