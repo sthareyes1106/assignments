119941139f55824c13f3f8bd3e83b805e8117363
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-29 16:26:22 -0500

    Added Post class

--- modules/default-export-and-import/Post.mjs
+++ modules/default-export-and-import/Post.mjs
@@ -1,1 +1,11 @@
 // Default Post class
+export class Post {
+  constructor(title, content) {
+    title;
+    content;
+  }
+
+  publish() {
+    console.log(`Title: ${this.title} \n\nContent: ${this.content}`);
+  }
+}
