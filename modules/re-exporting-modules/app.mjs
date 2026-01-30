598456af0e38881bafada2a74bc4cd7627ba2849
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-30 10:42:19 -0500

    Imported * to app.mjs and logged a few examples

--- modules/re-exporting-modules/app.mjs
+++ modules/re-exporting-modules/app.mjs
@@ -1,1 +1,14 @@
 // imports utilities from the aggregated utils/index.mjs and demonstrates use
+
+import {
+  capitalizeWords,
+  reverseWords,
+  addTwoNums,
+  findAvg,
+} from "./utils/index.mjs";
+
+console.log(capitalizeWords("wow a sentence"));
+console.log(reverseWords("wOwza a wEirD seNteNCe"));
+
+console.log(addTwoNums(3, 76));
+console.log(findAvg(4, 6, 1, 2, 9, 76, 23));
