dd073ce4aea2f84357bdfee3baabbfa2ee44330c
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-30 10:55:35 -0500

    Added folder dynamic-imports and theme.mjs file

---
+++ modules/dynamic-imports/theme.mjs
@@ -0,0 +1,15 @@
+// Exports functions that manage the applications theme
+
+let theme = null;
+
+// setLightTheme
+const setLightTheme = () => {
+  console.log("Theme: Light");
+  theme = "light";
+};
+
+// setDarkTheme
+const setDarkTheme = () => {
+  console.log("Theme: Dark");
+  theme = "dark";
+};
