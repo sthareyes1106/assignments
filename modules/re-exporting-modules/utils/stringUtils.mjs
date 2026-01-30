54399a1e7278dcb42c5bf7b8e2c39d1b0b883943
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-30 10:16:56 -0500

    Added capitalizeWords and reverseWords functions

--- modules/re-exporting-modules/utils/stringUtils.mjs
+++ modules/re-exporting-modules/utils/stringUtils.mjs
@@ -1,1 +1,17 @@
-// for string manipulation functions
+// do 1-2 functions for string manipulation
+// to capitalize words
+function capitalizeWords(string) {
+  return string
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+// to reverse words in a sentence because why not
+// similar to capitalizeWords above
+function reverseWords(sentence) {
+  return sentence
+    .split(" ") // make into array
+    .map((word) => word.split("").reverse().join("")) // reverse letters
+    .join(" "); // join into array
+}
