c9ff2157eee0b846cb938ad089c4f936bd979750
sthareyes1106 <sjrreyes06@gmail.com>
2026-01-30 10:34:06 -0500

    Added addTwoNums and findAvg functions

--- modules/re-exporting-modules/utils/numberUtils.mjs
+++ modules/re-exporting-modules/utils/numberUtils.mjs
@@ -1,1 +1,14 @@
 // for numeric operations
+// add two numbers together
+function addTwoNums(x, y) {
+  return x + y;
+}
+
+// find average
+function findAvg(...nums) {
+  // add all nums together
+  const total = nums.reduce(
+    (accumulator, currentNum) => accumulator + currentNum,
+  );
+  return total / nums.length;
+}
